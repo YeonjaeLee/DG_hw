0214d91571558bff51bf9791d6ce9c535cc01e00
Yeonjae Lee <leeyeonjae@wdiamondgames.com>
2017-10-24 10:17:14 +0900

    cs 위치변경

---
+++ UIPopup_Update1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIPopup_Update1 : UIPopup
+{
+    #region VARIABLES
+    private Text _txtInfo;
+    #endregion
+
+
+    #region METHODS - reserved
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _txtInfo = base.Find<Text>("Dialog/Text");
+    }
+
+    public override void Init(params object[] args_)
+    {
+        base.Init(args_);
+
+        if (args_.IsNullOrEmpty())
+            return;
+
+        _txtInfo.text = (string)args_[0];
+    }
+    #endregion
+
+
+    #region METHODS - base
+    public override void Show()
+    {
+        base.Show();
+        base.SetSizedelta();
+    }
+    #endregion
+}
